76faae5fd251643a8b234026d0e270c7f8362b7b
samda90 <samd902016@gmail.com>
2022-07-11 05:38:44 -0700

    printf_work

---
+++ print_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_string - Prints formatted string
+ *
+ *@argp: Argument parameters
+ *
+ *Return: return int.
+ */
+
+char print_string(va_list argp)
+{
+	char *s = va_arg(argp, char *);
+	int len = 0;
+
+	if (s == NULL)
+		s = "(nill)";
+	while (*s)
+	{
+		_putchar(*s);
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+
