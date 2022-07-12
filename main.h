40d801758122751f855c10f639b538744c6bf10a
eebbroo <majamibrahim@gmail.com>
2022-07-12 13:09:56 -0700

    exe file adde

--- main.h
+++ main.h
@@ -1,46 +1,61 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <limits.h>
-
-int _printf(const char *, ...);
-int print_str(va_list);
-char _putchar(char);
-
-/** prv - struct typedef
- *
- * @id: character var.
- * @f: A function pointer
+#include <unistd.h>
+/**
+ * struct prv - struct prv
+ * @id: chat
+ * @f: function pointer
  */
+
 typedef struct prv
 {
 	char id;
 	int (*f)(va_list args);
-}prv_t;
-int (*get_function(const char))(va_list);
-int print_char(va_list);
-int print_bin(va_list);
-int cnvrt_to_bin(unsigned int, int *);
-int print_dec(va_list);
-int cnvrt_to_dec(int, int *);
-int pr_un_int(va_list);
-int cnvrt_to_un_int(unsigned int, int *);
-int pr_octa(va_list);
-int cnvrt_to_octa(unsigned int, int *);
-int pr_hex(va_list);
-int cnvrt_to_hex(unsigned int, int *);
-int pr_heX(va_list);
-int cnvrt_to_heX(unsigned int, int *);
-int get_len(int, int);
-int pr_non_prt(va_list);
-int pr_rev(va_list);
-int get_str_len(char *);
-int pr_rot13(va_list);
-int pr_ptr(va_list);
+} prv_t;
+
+
+int _printf(const char *format, ...);
+
+int (*get_function(const char id))(va_list);
+
+int _putchar(int ch);
+
+int pr_char(va_list args);
+
+int pr_str(va_list args);
+
+int pr_bin(va_list args);
+int cnvrt_to_bin(unsigned int n, int *cnt);
+
+int pr_dec(va_list args);
+int cnvrt_to_dec(int n, int *cnt);
+
+int pr_un_int(va_list args);
+int cnvrt_to_un_int(unsigned int n, int *cnt);
+
+int pr_octa(va_list args);
+int cnvrt_to_octa(unsigned int n, int *cnt);
+
+int pr_hex(va_list args);
+int cnvrt_to_hex(unsigned int n, int *cnt);
+
+int pr_heX(va_list args);
+int cnvrt_to_heX(unsigned int n, int *cnt);
+
+int get_base_len(int n, int b);
+
+int pr_non_prt(va_list args);
+
+int pr_rev(va_list args);
+int get_str_len(char *s);
+
+int pr_rot13(va_list args);
+
+int pr_ptr(va_list args);
 
 #endif
